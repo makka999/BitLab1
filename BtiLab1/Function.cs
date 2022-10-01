63e29771837d50de65dc522a4a39c0b3722e29a2
makka999 <mateuszk-98@wp.pl>
2022-10-01 15:21:24 +0200

    dziala do poprawy

---
+++ BtiLab1/Function.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BtiLab1
+{
+    internal class Function
+    {
+        public static string Encrypt(char[] textToEncrypt, char[] alphabet, int key)
+        {
+            int alphabetLenght = alphabet.Length;
+            int length = textToEncrypt.Length;
+            char[] encryptedChar = new char[length];
+            for (int i = 0; i < textToEncrypt.Length; i++)
+            {
+                if (!(textToEncrypt[i] == ' '))
+                {
+                    var letter = textToEncrypt[i];
+                    int index = Array.IndexOf(alphabet, letter);
+                    int newIndex = (key + index) % alphabetLenght;
+                    if (newIndex < 0) newIndex = alphabetLenght + newIndex;
+                    char newLetter = alphabet[newIndex];
+                    encryptedChar[i] = newLetter;
+                }
+                else encryptedChar[i] = textToEncrypt[i];
+            }
+
+            string encrypToText = String.Join("", encryptedChar);
+            return encrypToText;
+        }
+
+        public static string UnEncrypt (char[] textToEncrypt, char[] alphabet, int key)
+        {
+            return Encrypt(textToEncrypt, alphabet, key * -1);
+        }
+
+    }
+}
---
+++ BtiLab1/Program.cs
@@ -0,0 +1,63 @@
+﻿using BtiLab1;
+
+
+char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+int switch_on = 0;
+
+Console.WriteLine("Menu");
+Console.WriteLine("1 - Szyfrowanie");
+Console.WriteLine("2 - Odszyfrowywanie");
+Console.WriteLine("3 - Odszyfrowywanie bez kodu");
+
+switch_on = Convert.ToInt32(Console.ReadLine());
+
+
+switch (switch_on)
+{
+    case 0:
+        
+
+    case 1:
+        Console.WriteLine("Wpisz wiadomosc");
+        string text = Console.ReadLine();
+        Console.WriteLine("Wpisz klucz");
+        int key = Convert.ToInt32(Console.ReadLine());
+        text = text.ToLower();
+        char[] charText = text.ToCharArray();
+        string cryptText = Function.Encrypt(charText, alphabet, key);
+        Console.WriteLine(cryptText);
+        break;
+    case 2:
+        Console.WriteLine("Wpisz wiadomosc");
+        string textD = Console.ReadLine();
+        Console.WriteLine("Wpisz klucz");
+        int keyD = Convert.ToInt32(Console.ReadLine());
+        textD = textD.ToLower();
+        char[] charTextD = textD.ToCharArray();
+        string cryptTextD = Function.UnEncrypt(charTextD, alphabet, keyD);
+        Console.WriteLine(cryptTextD);
+        break;
+    case 3:
+        Console.WriteLine("Wpisz wiadamosc");
+        string textDK = Console.ReadLine();
+        textDK = textDK.ToLower();
+        char[] charTextDK = textDK.ToCharArray();
+        for (int i = 0; i < alphabet.Length; i++)
+        {
+            string cryptTextDK = Function.Encrypt(charTextDK, alphabet, i);
+            Console.WriteLine(cryptTextDK);
+        }
+
+        break;
+
+}
+
+
+
+
+
+
+
+
+
